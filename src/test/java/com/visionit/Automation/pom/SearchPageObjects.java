12cd4b547511fe791dbaa330e93fc34c9d0ff68c
amrutakdesai <97360236+amrutakdesai@users.noreply.github.com>
2022-03-11 14:59:29 +0530

    Modify the SearchPageObjects

--- src/test/java/com/visionit/Automation/pom/SearchPageObjects.java
+++ src/test/java/com/visionit/Automation/pom/SearchPageObjects.java
@@ -13,30 +13,27 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 public class SearchPageObjects {
 	private static final Logger logger = LogManager.getLogger(SearchPageObjects.class);
 
-	private WebDriver driver;
+		private WebDriver driver;
 	private By searchBox=By.id("search_query_top");
-	private By searchButton = By.name("submit_search");
-	private By searchResult=By.xpath("//div[@id='center_column']/h1/span");
+	//private By searchButton = By.name("submit_search");
+	private By searchResult=By.xpath("//div[@class='ac_results']/ul/li/strong[contains(text(),'T-shirt')]");
 
 	//Section 3: Paratmerize the constuctor
 	public SearchPageObjects(WebDriver driver){
 		this.driver = driver;
 	}
 	
-	public void SearchBoxFunctionality(String Pname)
+	public void SearchBoxFunctionality(String Pname) throws InterruptedException
 	{
 		WebDriverWait webDriverWait = new WebDriverWait(driver,20);
 		WebElement elementSearchBox = webDriverWait.until(ExpectedConditions.elementToBeClickable(searchBox));
 		elementSearchBox.sendKeys(Pname);
+		Thread.sleep(2000);
+
 		logger.info("Value entered in search box: " + Pname);
 	
 	}
-	public void ClickOnSearchButton() 
-	{
-		driver.findElement(searchButton).click();
-		logger.info("Clicked on Search Button");
-	}
-
+	
 	public void ValidateProductSearchResult(String Pname)
 	{
 		String displayResult=driver.findElement(searchResult).getText();
